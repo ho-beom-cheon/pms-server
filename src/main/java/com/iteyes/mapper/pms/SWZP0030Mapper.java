c9b7315c0f6284cae0713696f3216e7d634a605f
hbcheon <hbcheon@iteyes.co.kr>
2021-12-21 17:29:56 +0900

    #5 DB서버 변경

--- src/main/java/com/iteyes/mapper/pms/SWZP0030Mapper.java
+++ src/main/java/com/iteyes/mapper/pms/SWZP0030Mapper.java
@@ -9,7 +9,4 @@ import com.iteyes.dto.pms.SWZP0030DTO;
 @Mapper
 public interface SWZP0030Mapper {
 	List<SWZP0030DTO> select_0030_01(SWZP0030DTO SWZP0030) throws Exception;
-	List<SWZP0030DTO> update_0030_01(SWZP0030DTO SWZP0030) throws Exception;
-
-
 }
--- src/main/java/com/iteyes/service/SWZP0030Service.java
+++ src/main/java/com/iteyes/service/SWZP0030Service.java
@@ -6,5 +6,4 @@ import com.iteyes.dto.pms.SWZP0030DTO;
 
 public interface SWZP0030Service {
 	List<SWZP0030DTO> select_0030(SWZP0030DTO SWZP0030) throws Exception ;
-	List<SWZP0030DTO> update_0030(SWZP0030DTO SWZP0030) throws Exception ;
 }
