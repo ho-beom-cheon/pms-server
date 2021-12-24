3f9e6cb2b1cb20b664477429059b7276e4b64e86
ghjeong <ghjeong@iteyes.co.kr>
2021-12-24 16:59:56 +0900

    #12 SWZP0060 ActionItem및이슈관리현황 조회구현

---
+++ src/main/java/com/iteyes/mapper/pms/SWZP0060Mapper.java
@@ -0,0 +1,11 @@
+package com.iteyes.mapper.pms;
+
+import com.iteyes.dto.pms.SWZP0060DTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SWZP0060Mapper {
+	List<SWZP0060DTO> select_0060_01(SWZP0060DTO SWZP0060) throws Exception;
+}
