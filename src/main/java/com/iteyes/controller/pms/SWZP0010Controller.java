d7dd656a513dd453f4303057402e5d605332eb0e
hbcheon <hbcheon@iteyes.co.kr>
2022-01-04 13:37:36 +0900

    # SWZP0010Controller 소스 중복 수정

--- src/main/java/com/iteyes/controller/pms/SWZP0010Controller.java
+++ src/main/java/com/iteyes/controller/pms/SWZP0010Controller.java
@@ -33,47 +33,23 @@ import java.util.Map;
 @CrossOrigin("*")
 @RequestMapping(value = "/SWZP0010")
 public class SWZP0010Controller {
-	@Autowired
-	private SWZP0010Service swzp0010Service;
+    @Autowired
+    private SWZP0010Service swzp0010Service;
 
-	@GetMapping(value = "/select")
+    @GetMapping(value = "/select")
     public @ResponseBody String select(HttpServletRequest request) throws Exception{
 
-		ObjectMapper mapper = new ObjectMapper();
-    	
-    	log.debug("# pgm_id :: "   + request.getParameter("pgm_id"));
-    	log.debug("# pgm_nm :: "   + request.getParameter("pgm_nm"));
-    	log.debug("# pl_no :: "    + request.getParameter("pl_no"));
-    	log.debug("# dvlpe_no :: " + request.getParameter("dvlpe_no"));
-    	log.debug("# pgm_dis_cd :: " + request.getParameter("pgm_dis_cd_selected"));
-    	log.debug("# bzcd :: " + request.getParameter("bzcd_selected"));
-    	log.debug("# dvlp_dis_cd :: " + request.getParameter("dvlp_dis_cd_selected"));
-    	
-    	SWZP0010DTO SWZP0010 = new SWZP0010DTO();
-    	
-    	SWZP0010.setPgm_id(request.getParameter("pgm_id"));
-    	SWZP0010.setPgm_nm(request.getParameter("pgm_nm"));
-    	SWZP0010.setPl_no(request.getParameter("pl_no"));
-    	SWZP0010.setDvlpe_no(request.getParameter("dvlpe_no"));
-    	SWZP0010.setPgm_dis_cd(request.getParameter("pgm_dis_cd_selected"));
-    	SWZP0010.setBzcd(request.getParameter("bzcd_selected"));
-    	SWZP0010.setDvlp_dis_cd(request.getParameter("dvlp_dis_cd_selected"));
-    	
-    	List<SWZP0010DTO> list = swzp0010Service.select_0010(SWZP0010);
-    	
-    	HashMap<String, Object> hm = new HashMap();
-    	HashMap<String, Object> hm1 = new HashMap();
-    	HashMap<String, Object> hm1_pagination = new HashMap();
-    	hm.put("result", true);
-    	hm1.put("contents", list);
-    	hm1_pagination.put("page", 1);
-    	hm1_pagination.put("totalCount", 100);
-    	hm1.put("pagination", hm1_pagination);
-    	hm.put("data", hm1);
+        ObjectMapper mapper = new ObjectMapper();
 
-    	String jsonStr = mapper.writeValueAsString(hm);
+        log.debug("# pgm_id :: "   + request.getParameter("pgm_id"));
+        log.debug("# pgm_nm :: "   + request.getParameter("pgm_nm"));
+        log.debug("# pl_no :: "    + request.getParameter("pl_no"));
+        log.debug("# dvlpe_no :: " + request.getParameter("dvlpe_no"));
+        log.debug("# pgm_dis_cd :: " + request.getParameter("pgm_dis_cd_selected"));
+        log.debug("# bzcd :: " + request.getParameter("bzcd_selected"));
+        log.debug("# dvlp_dis_cd :: " + request.getParameter("dvlp_dis_cd_selected"));
 
-    	return jsonStr;
+        SWZP0010DTO SWZP0010 = new SWZP0010DTO();
 
         /* 서비스 요청 */
         List<SWZP0010DTO> list = swzp0010Service.select_0010(SWZP0010);
