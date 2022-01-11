c6caae96ffe0978310d433d01687a20e89f7d5e1
ghjeong <ghjeong@iteyes.co.kr>
2022-01-11 16:10:16 +0900

    #25 SWZP0030 통합테스트 저장 기능 구현 - SWZP0030 통합테스트 저장 기능 구현 (수정필요) - SWZP0060 ActionItem및이슈관리현황 관리ID 생성 쿼리 수정

--- src/main/java/com/iteyes/controller/pms/SWZP0030Controller.java
+++ src/main/java/com/iteyes/controller/pms/SWZP0030Controller.java
@@ -1,73 +1,52 @@
 package com.iteyes.controller.pms;
 
-import java.util.HashMap;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iteyes.dto.pms.SWZP0030DTO;
 import com.iteyes.service.SWZP0030Service;
-
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+
+@RestController
 @Log4j2
-@RequestMapping(value = "/SWZP0030", method = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin("*")
+@RequestMapping(value = "/SWZP0030")
 public class SWZP0030Controller {
-	@Autowired
-	private SWZP0030Service swzp0030Service;
+    @Autowired
+    private SWZP0030Service swzp0030Service;
 
-	@GetMapping(value = "/select")
-	public @ResponseBody String select(HttpServletRequest request) throws Exception{
+    @GetMapping(value = "/select")
+    public @ResponseBody
+    String select(HttpServletRequest request) throws Exception {
 
-		ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
 
-		log.debug("# pgm_id :: "   + request.getParameter("scnr_id"));
-		log.debug("# tst_case_id :: "   + request.getParameter("tst_case_id"));
-		log.debug("# dvlpe_eno :: "    + request.getParameter("dvlpe_eno"));
-		log.debug("# pl_eno :: " + request.getParameter("pl_eno"));
-		log.debug("# crpe_eno :: " + request.getParameter("crpe_eno"));
-		log.debug("# rqu_sbh_id :: " + request.getParameter("rqu_sbh_id"));
-		log.debug("# prjt_nm :: " + request.getParameter("prjt_nm_selected"));
-		log.debug("# bzcd_ :: " + request.getParameter("bzcd_selected"));
-		log.debug("# sqn_cd :: " + request.getParameter("sqn_cd_selected"));
-		log.debug("# itg_tst_prc_cd :: " + request.getParameter("itg_tst_prc_cd_selected"));
-		log.debug("# tp :: " + request.getParameter("tp_selected"));
+        SWZP0030DTO SWZP0030 = new SWZP0030DTO();
+        
+        /* 서비스 요청 */
+        List<SWZP0030DTO> list = swzp0030Service.select_0030(SWZP0030);
 
-		SWZP0030DTO SWZP0030 = new SWZP0030DTO();
+        /* map 형태로 저장 */
+        HashMap<String, Object> hm = new HashMap();
+        HashMap<String, Object> hm1 = new HashMap();
+        HashMap<String, Object> hm1_pagination = new HashMap();
+        hm.put("result", true);
+        hm1.put("contents", list);
+        hm1_pagination.put("page", 1);
+        hm1_pagination.put("totalCount", 100);
+        hm1.put("pagination", hm1_pagination);
+        hm.put("data", hm1);
 
-		SWZP0030.setScnr_id(request.getParameter("scnr_id"));
-		SWZP0030.setTst_case_id(request.getParameter("tst_case_id"));
-		SWZP0030.setDvlpe_eno(request.getParameter("dvlpe_eno"));
-		SWZP0030.setPl_eno(request.getParameter("pl_eno"));
-		SWZP0030.setCrpe_eno(request.getParameter("crpe_eno"));
-		SWZP0030.setRqu_sbh_id(request.getParameter("rqu_sbh_id"));
-		SWZP0030.setPrjt_nm(request.getParameter("prjt_nm_selected"));
-		SWZP0030.setBzcd(request.getParameter("bzcd_selected"));
-		SWZP0030.setSqn_cd(request.getParameter("sqn_cd_selected"));
-		SWZP0030.setItg_tst_prc_cd(request.getParameter("itg_tst_prc_cd_selected"));
-		SWZP0030.setTp(request.getParameter("tp_selected"));
+        /* json 형태로 반환 */
+        String jsonStr = mapper.writeValueAsString(hm);
 
-		List<SWZP0030DTO> list = swzp0030Service.select_0030(SWZP0030);
+        return jsonStr;
 
-		HashMap<String, Object> hm = new HashMap();
-		HashMap<String, Object> hm1 = new HashMap();
-		HashMap<String, Object> hm1_pagination = new HashMap();
-		hm.put("result", true);
-		hm1.put("contents", list);
-		hm1_pagination.put("page", 1);
-		hm1_pagination.put("totalCount", 100);
-		hm1.put("pagination", hm1_pagination);
-		hm.put("data", hm1);
-
-		String jsonStr = mapper.writeValueAsString(hm);
-
-		return jsonStr;
-
-	}
+    }
 }
--- src/main/java/com/iteyes/mapper/pms/SWZP0030Mapper.java
+++ src/main/java/com/iteyes/mapper/pms/SWZP0030Mapper.java
@@ -9,4 +9,10 @@ import com.iteyes.dto.pms.SWZP0030DTO;
 @Mapper
 public interface SWZP0030Mapper {
 	List<SWZP0030DTO> select_0030_01(SWZP0030DTO SWZP0030) throws Exception;
+	/*등록*/
+	boolean insert_0030_01(SWZP0030DTO SWZP0030) throws Exception ;
+	/*수정*/
+	boolean update_0030_01(SWZP0030DTO SWZP0030) throws Exception ;
+	/*삭제*/
+	boolean update_0030_02(SWZP0030DTO SWZP0030) throws Exception ;
 }
