a2aa65f6eb1d6dce1216f37594af03679cd93683
hbcheon <hbcheon@iteyes.co.kr>
2022-01-11 16:49:18 +0900

    #24 개발 현황 화면 추가 개발 - 화면ID 변경

---
+++ src/main/java/com/iteyes/dto/pms/PJTE2100DTO.java
@@ -0,0 +1,60 @@
+package com.iteyes.dto.pms;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PJTE2100DTO {
+
+	private String bzcd;                    // 업무구분코드
+	private String bz_dtls_txt;             // 업무세부내용
+	private String pgm_id;                  // 프로그램id
+	private String pgm_nm;                  // 프로그램명
+	private String dvlp_dis_cd;             // 개발구분코드
+	private String pgm_dis_cd;              // 프로그램구분코드
+	private String enlpe_nm;                // 프로그램세부구분코드
+	private String frcs_sta_dt;             // 예상시작일자
+	private String frcs_end_dt;             // 예상종료일자
+	private String sta_dt;                  // 실제시작일자
+	private String end_dt;                  // 실제종료일자
+	private String dvlpe_cnf_dt;            // 개발자확인일자
+	private String pl_cnf_dt;               // pl확인일자
+	private String prc_step_cd;             // 처리단계코드
+	private String prg_txt;                 // 진행현황
+	private String dvlpe_nm;                // 개발자명
+	private String pl_nm;                   // PL명
+	private String crpe_nm;                 // 담당자명
+	private int err_tot_cnt;                // 총 건수
+	private int err_cmpl_cnt;               //결함완료 건수
+	private int err_ncmpl_cnt;             //결함진행 건수
+	private String atfl_mng_id;             // 단위테스트결과서 첨부파일관리ID
+	private String pal_atfl_mng_id;         // 설계서 첨부파일관리ID
+	private String rmrk;                    // 비고
+	private String dvlpe_no;                // 개발자번호
+	private String pl_no;                   // PL번호
+	private String crpe_no;                 // 담당자번호
+	private String save_yn;                 // 등록여부
+	private String bkup_id;                 // 백업id
+	private String prjt_id;                 // 프로젝트id
+	private String grp_tycd;                //
+    private String dtls_tycd;               //
+    private String dtls_tynm;               //
+    private String sort_seq;                //
+	private String dvlpe_sta_dt;            // 개발시작일자
+	private String dvlpe_end_dt;            // 개발종료일자
+
+	private List<PJTE2100DTO> updatedRows;
+	private List<PJTE2100DTO> createdRows;
+	private List<PJTE2100DTO> deletedRows;
+
+	private String mng_id;                  // 관리ID
+
+	private String bz_dtls_cd;			    // 업무세부코드
+
+	private String db_chg_ts;               // db변경일시
+	private String aut_id;                  // 권한
+	private String empno;
+	private String array_length;
+
+}
---
+++ src/main/java/com/iteyes/service/impl/pms/PJTE2100ServiceImpl.java
@@ -0,0 +1,49 @@
+package com.iteyes.service.impl.pms;
+
+import java.util.List;
+
+import com.iteyes.dto.pms.PJTE2100DTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iteyes.mapper.pms.PJTE2100Mapper;
+import com.iteyes.service.PJTE2100Service;
+
+@Service
+public class PJTE2100ServiceImpl implements PJTE2100Service {
+	@Autowired
+	private PJTE2100Mapper pjte2100Mapper;
+
+	@Override
+	public List<PJTE2100DTO> select_0000(PJTE2100DTO PJTE2100) throws Exception {
+
+		return pjte2100Mapper.select_0000_01(PJTE2100);
+	}
+
+	@Override
+	public List<PJTE2100DTO> select_2100_01(PJTE2100DTO PJTE2100) throws Exception {
+
+		return pjte2100Mapper.select_2100_01(PJTE2100);
+	}
+
+	/* create */
+	@Override
+	public boolean insert_2100_01(PJTE2100DTO PJTE2100) throws Exception {
+
+		return pjte2100Mapper.insert_2100_01(PJTE2100);
+	}
+
+	/* update */
+	@Override
+	public boolean update_2100_01(PJTE2100DTO PJTE2100) throws Exception {
+
+		return pjte2100Mapper.update_2100_01(PJTE2100);
+	}
+
+	/* delete */
+	@Override
+	public boolean update_2100_02(PJTE2100DTO PJTE2100) throws Exception {
+
+		return pjte2100Mapper.update_2100_02(PJTE2100);
+	}
+}
