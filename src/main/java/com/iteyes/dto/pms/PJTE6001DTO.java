4f90d4f96da7a98233d2d01d800f832d7d873b01
moon <jn5574@iteyes.co.kr>
2022-01-21 13:32:10 +0900

    #35 PJTE6001 PMS신청등록 작업

---
+++ src/main/java/com/iteyes/dto/pms/PJTE6001DTO.java
@@ -0,0 +1,25 @@
+package com.iteyes.dto.pms;
+
+import lombok.Data;
+
+@Data
+public class PJTE6001DTO {
+	private String prjt_id;
+	private String mng_id;
+	private String bzcd;
+	private String req_dscd;
+	private String reqpe_no;
+	private String req_dt;
+	private String req_txt;
+	private String prcpe_nm;
+	private String prcpe_no;
+	private String prc_dt;
+	private String prc_txt;
+	private String req_prc_step_cd;
+	private String atfl_mng_id;
+	private String opr_no;
+
+	private String bkup_id;
+	private String org_file_nm;
+	private String login_emp_no;
+}
---
+++ src/main/java/com/iteyes/mapper/pms/PJTE6001Mapper.java
@@ -0,0 +1,14 @@
+package com.iteyes.mapper.pms;
+
+import com.iteyes.dto.pms.PJTE6001DTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PJTE6001Mapper {
+	List<PJTE6001DTO> select_6001_01(PJTE6001DTO PJTE6001) throws Exception;
+	boolean insert_6001_01(PJTE6001DTO PJTE6001) throws Exception;
+	boolean update_6001_01(PJTE6001DTO PJTE6001) throws Exception;
+
+}
