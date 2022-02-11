855cb6dca20d32e6a263b21b0af7c43b49312d47
ghjeong <ghjeong@iteyes.co.kr>
2022-02-11 12:38:39 +0900

    #44 공통적용사항 적용 및 화면 오류수정 1. PJTE3001 - 결함 신규 등록 시 개발자 및 PL조회 쿼리 추가

--- src/main/java/com/iteyes/service/PJTE3001Service.java
+++ src/main/java/com/iteyes/service/PJTE3001Service.java
@@ -12,8 +12,10 @@ public interface PJTE3001Service {
     List<PJTE3001DTO> select_0000_03(PJTE3001DTO PJTE3001) throws Exception ;
     List<PJTE3001DTO> select_0000_04(PJTE3001DTO PJTE3001) throws Exception ;
 
-    /* select */
+    /* mng_id가 있을 때 팝업 데이터 조회 select */
     List<PJTE3001DTO> select_3001_01(PJTE3001DTO PJTE3001) throws Exception ;
+    /* mng_id가 없을 때 신규 등록을 위한팝업 데이터 조회 select */
+    List<PJTE3001DTO> select_3001_02(PJTE3001DTO PJTE3001) throws Exception ;
 
     /* update */
     boolean update_3001_01(PJTE3001DTO PJTE3001) throws Exception;
