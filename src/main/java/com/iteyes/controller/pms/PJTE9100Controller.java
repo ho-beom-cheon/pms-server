a55832afe785245155fa6d4fdabc3bbb1b884597
yyr <year982@iteyes.co.kr>
2022-04-11 18:05:26 +0900

    #70 PJTE9100 KMS게시판생성 신규 개발 - 서버 개발

--- src/main/java/com/iteyes/controller/pms/PJTE9100Controller.java
+++ src/main/java/com/iteyes/controller/pms/PJTE9100Controller.java
@@ -32,8 +32,8 @@ public class PJTE9100Controller {
         /* dto 값 셋팅*/
         PJTE9100.setPrjt_id(request.getParameter("prjt_nm_selected"));
         PJTE9100.setBkup_id(request.getParameter("bkup_id_selected"));
-        PJTE9100.setBubun_cd(request.getParameter("bubun_cd"));
-        PJTE9100.setBsn_cls_cd(request.getParameter("bsn_cls_cd"));
+        PJTE9100.setBubun_cd(request.getParameter("bubun_cd_selected"));
+        PJTE9100.setBsn_cls_cd(request.getParameter("bsn_cls_cd_selected_iss"));
         PJTE9100.setGesipan_titl(request.getParameter("gesipan_titl"));
 
         /* 서비스 요청**/
@@ -61,9 +61,27 @@ public class PJTE9100Controller {
 
         boolean result = false;
 
-        ObjectMapper mapper = new ObjectMapper();
+        PJTE9100DTO PJTE9100U = new PJTE9100DTO();
 
-        result = pjte9100Service.update_9100_01(PJTE9100);
+        PJTE9100U.setBkup_id     (PJTE9100.getBkup_id      ());
+        PJTE9100U.setPrjt_id     (PJTE9100.getPrjt_id      ());
+        PJTE9100U.setGesipan_id  (PJTE9100.getGesipan_id   ());    //게시판ID
+
+        PJTE9100U.setBubun_cd     (PJTE9100.getBubun_cd     ());    //부문코드
+        PJTE9100U.setBsn_cls_cd   (PJTE9100.getBsn_cls_cd   ());    //업무구분코드
+        PJTE9100U.setGesipan_titl (PJTE9100.getGesipan_titl ());    //게시판제목
+        PJTE9100U.setGesipan_dsc  (PJTE9100.getGesipan_dsc  ());    //게시판설명
+        PJTE9100U.setAnnym_yn     (PJTE9100.getAnnym_yn     ());    //익명여부
+        PJTE9100U.setCmnt_yn      (PJTE9100.getCmnt_yn      ());    //댓글여부
+        PJTE9100U.setRply_yn      (PJTE9100.getRply_yn      ());    //답글여부
+        PJTE9100U.setGood_yn      (PJTE9100.getGood_yn      ());    //좋아요여부
+        PJTE9100U.setAfrm_yn      (PJTE9100.getAfrm_yn      ());    //소속확인여부
+        PJTE9100U.setNmb_inq_yn   (PJTE9100.getNmb_inq_yn   ());    //조회횟수여부
+        PJTE9100U.setPgn_yn       (PJTE9100.getPgn_yn       ());    //페이징여부
+        PJTE9100U.setFile_upld_yn (PJTE9100.getFile_upld_yn ());    //파일업로드여부
+        PJTE9100U.setLogin_emp_no (PJTE9100.getLogin_emp_no ());    //직원번호
+
+        result = pjte9100Service.update_9100_01(PJTE9100U);
 
         return result;
 
@@ -74,28 +92,25 @@ public class PJTE9100Controller {
     boolean create(HttpServletRequest request, @RequestBody PJTE9100DTO PJTE9100) throws Exception {
         boolean result = false;
 
-        PJTE9100DTO PJTE9100D = new PJTE9100DTO();
-
-        PJTE9100D.setPrjt_id     (PJTE9100.getPrjt_id());
-        PJTE9100D.setBkup_id(PJTE9100.getBkup_id());
-
         PJTE9100DTO PJTE9100C = new PJTE9100DTO();
 
-        PJTE9100C.setPrjt_id      (PJTE9100.getPrjt_id      ());
-        PJTE9100C.setLogin_emp_no (PJTE9100.getLogin_emp_no ());
-        PJTE9100C.setBubun_cd     (PJTE9100.getBubun_cd     ());
-        PJTE9100C.setBsn_cls_cd   (PJTE9100.getBsn_cls_cd   ());
-        PJTE9100C.setGesipan_titl (PJTE9100.getGesipan_titl ());
-        PJTE9100C.setNotice_sta_dt(PJTE9100.getNotice_sta_dt());
-        PJTE9100C.setAnnmt_yn     (PJTE9100.getAnnmt_yn     ());
-        PJTE9100C.setComment_yn   (PJTE9100.getComment_yn   ());
-        PJTE9100C.setNotice_titl  (PJTE9100.getNotice_titl  ());
-        PJTE9100C.setGesipan_id   (PJTE9100.getGesipan_id   ());
-        PJTE9100C.setGesipan_dsc  (PJTE9100.getGesipan_dsc  ());
-        PJTE9100C.setAfrm_yn      (PJTE9100.getAfrm_yn      ());
-        PJTE9100C.setNmb_inq_yn   (PJTE9100.getNmb_inq_yn   ());
-        PJTE9100C.setPgn_yn       (PJTE9100.getPgn_yn       ());
-        PJTE9100C.setFile_upld_yn (PJTE9100.getFile_upld_yn ());
+        PJTE9100C.setPrjt_id     (PJTE9100.getPrjt_id());
+        PJTE9100C.setBkup_id     (PJTE9100.getBkup_id());
+
+        PJTE9100C.setLogin_emp_no (PJTE9100.getLogin_emp_no ());    //직원번호
+        PJTE9100C.setBubun_cd     (PJTE9100.getBubun_cd     ());    //부문코드
+        PJTE9100C.setBsn_cls_cd   (PJTE9100.getBsn_cls_cd   ());    //업무구분코드
+        PJTE9100C.setGesipan_titl (PJTE9100.getGesipan_titl ());    //게시판제목
+        PJTE9100C.setGood_yn      (PJTE9100.getGood_yn      ());    //좋아요여부
+        PJTE9100C.setAnnym_yn     (PJTE9100.getAnnym_yn     ());    //익명여부
+        PJTE9100C.setCmnt_yn      (PJTE9100.getCmnt_yn      ());    //댓글여부
+        PJTE9100C.setRply_yn      (PJTE9100.getRply_yn      ());    //답글여부
+        PJTE9100C.setGesipan_id   (PJTE9100.getGesipan_id   ());    //게시판ID
+        PJTE9100C.setGesipan_dsc  (PJTE9100.getGesipan_dsc  ());    //게시판설명
+        PJTE9100C.setAfrm_yn      (PJTE9100.getAfrm_yn      ());    //소속확인여부
+        PJTE9100C.setNmb_inq_yn   (PJTE9100.getNmb_inq_yn   ());    //조회횟수여부
+        PJTE9100C.setPgn_yn       (PJTE9100.getPgn_yn       ());    //페이징여부
+        PJTE9100C.setFile_upld_yn (PJTE9100.getFile_upld_yn ());    //파일업로드여부
 
         result = pjte9100Service.insert_9100_01(PJTE9100C);
 
--- src/main/java/com/iteyes/dto/pms/PJTE9100DTO.java
+++ src/main/java/com/iteyes/dto/pms/PJTE9100DTO.java
@@ -13,19 +13,21 @@ public class PJTE9100DTO {
     private String login_catn_dcd;          // 구성원구분코드
     private String login_aut_cd;            // 권한 ID
 
-    private String bkup_id;                 // 백업ID
-    private String prjt_id;                 // 프로젝트ID
-    private String bubun_cd;                // 게시부문
-    private String bsn_cls_cd;              // 게시구분
-    private String gesipan_titl;            // 게시판제목
-    private String notice_sta_dt;           // 익명
-    private String annmt_yn;                // 댓글
-    private String comment_yn;              // 답글
-    private String notice_titl;             // 좋아요
-    private String gesipan_id;              // 게시판ID
-    private String gesipan_dsc;             // 게시판설명
-    private String afrm_yn;                 // 소속확인여부
-    private String nmb_inq_yn;              // 조회횟수여부
-    private String pgn_yn;                  // 페이징여부
-    private String file_upld_yn;            // 파일업로드여부
+    private String bkup_id;		            // 백업ID
+    private String prjt_id;		            // 프로젝트ID
+    private String gesipan_id;		        // 게시판ID
+    private String bubun_cd;		        // 부문코드
+    private String gesipan_titl;	        // 게시판제목
+    private String gesipan_dsc;	            // 게시판설명
+    private String bsn_cls_cd;		        // 업무구분코드
+    private String annym_yn;		        // 익명여부
+    private String afrm_yn;		            // 소속확인여부
+    private String cmnt_yn;		            // 댓글여부
+    private String rply_yn;		            // 답글여부
+    private String good_yn;		            // 좋아요여부
+    private String nmb_inq_yn;		        // 조회횟수여부
+    private String pgn_yn;			        // 페이징여부
+    private String file_upld_yn;	        // 파일업로드여부
+    private String opr_no;			        // 조작자번호
+    private String db_chg_ts;               // DB변경일시
 }
