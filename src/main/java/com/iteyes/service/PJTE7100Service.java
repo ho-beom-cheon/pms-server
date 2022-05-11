b285b81036c6444a3df472d2b0b22b019110d42d
HYUNGYU <hyungyu.shin@iteyes.co.kr>
2022-05-11 16:56:52 +0900

    #77 보완사항수정 서비스 추가

---
+++ src/main/java/com/iteyes/service/PJTE7100Service.java
@@ -0,0 +1,13 @@
+package com.iteyes.service;
+
+import com.iteyes.dto.pms.PJTE7100DTO;
+
+import java.util.List;
+
+public interface PJTE7100Service {
+	List<PJTE7100DTO> select_7100_01(PJTE7100DTO PJTE7100) throws Exception ;
+	List<PJTE7100DTO> select_7100_02(PJTE7100DTO PJTE7100) throws Exception ;
+
+	boolean update_7100_01(PJTE7100DTO PJTE7100) throws Exception;
+	boolean insert_7100_01(PJTE7100DTO PJTE7100) throws Exception;
+}
---
+++ src/main/java/com/iteyes/service/impl/pms/PJTE7100ServiceImpl.java
@@ -0,0 +1,44 @@
+package com.iteyes.service.impl.pms;
+
+import com.iteyes.dto.pms.PJTE7100DTO;
+import com.iteyes.mapper.pms.PJTE7100Mapper;
+import com.iteyes.service.PJTE7100Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PJTE7100ServiceImpl implements PJTE7100Service {
+    @Autowired
+    private PJTE7100Mapper pjte7100Mapper;
+
+    @Override
+    public List<PJTE7100DTO> select_7100_01(PJTE7100DTO PJTE7100) throws Exception {
+
+        return pjte7100Mapper.select_7100_01(PJTE7100);
+
+    }
+
+    @Override
+    public List<PJTE7100DTO> select_7100_02(PJTE7100DTO PJTE7100) throws Exception {
+
+        return pjte7100Mapper.select_7100_02(PJTE7100);
+
+    }
+
+    @Override
+    public boolean insert_7100_01(PJTE7100DTO PJTE7100) throws Exception {
+
+        return pjte7100Mapper.insert_7100_01(PJTE7100);
+
+    }
+
+    @Override
+    public boolean update_7100_01(PJTE7100DTO PJTE7100) throws Exception {
+
+        return pjte7100Mapper.update_7100_01(PJTE7100);
+
+    }
+
+}
