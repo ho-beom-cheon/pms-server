cf9e7604d5946c78cb1c1c902dc6d2f17bf25725
HYUNGYU <hyungyu.shin@iteyes.co.kr>
2022-05-16 17:59:11 +0900

    #77 보완사항수정 엑셀로드여부 비교문 수정

--- src/main/java/com/iteyes/controller/pms/PJTE2100Controller.java
+++ src/main/java/com/iteyes/controller/pms/PJTE2100Controller.java
@@ -116,7 +116,7 @@ public class PJTE2100Controller {
 
         PJTE2100DTO PJTE2100C = new PJTE2100DTO();
 
-        if(PJTE2100.getExcelUplod() == "Y") {
+        if(PJTE2100.getExcelUplod().equals("Y")) {
 
             PJTE2100DTO PJTE2100D = new PJTE2100DTO();
 
