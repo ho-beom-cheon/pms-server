330b8ed48be0647625d23547f6a93972a9095ccb
HYUNGYU <clover8091@naver.com>
2022-08-12 13:15:49 +0900

    #77 보완사항수정 엑셀 업로드 변경

--- src/main/java/com/iteyes/service/PJTE7100Service.java
+++ src/main/java/com/iteyes/service/PJTE7100Service.java
@@ -1,6 +1,5 @@
 package com.iteyes.service;
 
-import com.iteyes.dto.pms.PJTE2100DTO;
 import com.iteyes.dto.pms.PJTE7100DTO;
 
 import java.util.List;
@@ -8,8 +7,10 @@ import java.util.List;
 public interface PJTE7100Service {
 	List<PJTE7100DTO> select_7100_01(PJTE7100DTO PJTE7100) throws Exception ;
 	List<PJTE7100DTO> select_7100_02(PJTE7100DTO PJTE7100) throws Exception ;
+	List<PJTE7100DTO> select_7100_03(PJTE7100DTO PJTE7100) throws Exception ;
 
 	boolean update_7100_01(PJTE7100DTO PJTE7100) throws Exception;
+	boolean update_7100_02(PJTE7100DTO PJTE7100) throws Exception;
 	boolean insert_7100_01(PJTE7100DTO PJTE7100) throws Exception;
 
 	/*삭제*/
