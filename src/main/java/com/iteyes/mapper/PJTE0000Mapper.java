1af15365b5ca4ca01a1ac65477836b74858694f7
HYUNGYU <hyungyu.shin@iteyes.co.kr>
2023-03-17 10:49:00 +0900

    93-SHIN서비스수정반영 최종접속일시 추가

--- src/main/java/com/iteyes/mapper/PJTE0000Mapper.java
+++ src/main/java/com/iteyes/mapper/PJTE0000Mapper.java
@@ -2,6 +2,7 @@ package com.iteyes.mapper;
 
 import java.util.List;
 
+import com.iteyes.dto.pms.PJTE2100DTO;
 import com.iteyes.dto.pms.PJTE9000DTO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -14,4 +15,6 @@ public interface PJTE0000Mapper {
 	List<PJTE0000DTO> select_0120_02(PJTE0000DTO PJTE0000Dto) throws Exception;
 	List<PJTE9000DTO> select_9000_01(PJTE9000DTO swzp9000Dto) throws Exception;
 	boolean pw_change_0000(PJTE0000DTO user) throws Exception;
+
+	boolean update_0000_01(PJTE0000DTO PJTE0000) throws Exception ;
 }
